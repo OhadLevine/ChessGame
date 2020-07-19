b60acb48cf614d8d66677c9b77d3feaf89251f52
Ohad <ohadlevin99@gmail.com>
2020-07-19 21:22:34 +0300

    copied splash logic

--- app/src/main/java/ohad/levin/chess/Splash.java
+++ app/src/main/java/ohad/levin/chess/Splash.java
@@ -1,5 +1,7 @@
 package ohad.levin.chess;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,22 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new
+                            Intent(Splash.this, GameActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
     }
 }
